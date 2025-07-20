36724e262d6b0eb387fc3dbba23f9aa364e89f2d
Daniel-Brd <dev.danielbrandao@gmail.com>
2025-07-20 15:59:30 -0300

    [API] feat: add ConfigModule.forRoot in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,17 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users.module';
 import { TasksModule } from './modules/tasks.module';
 import { AuthModule } from './modules/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, TasksModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    UsersModule,
+    TasksModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
