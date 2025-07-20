b788170a36f182c4f7caa54661cc11965272713b
Daniel-Brd <dev.danielbrandao@gmail.com>
2025-07-20 19:37:09 -0300

    [API] feat: add reconstitute method to Task class and update ITaskRepository to include findAllByUserId

--- src/@core/domain/task/task.repository.ts
+++ src/@core/domain/task/task.repository.ts
@@ -1,5 +1,12 @@
+import { TaskFilters } from './task-filters.type';
+import { TaskOrderBy } from './task-order.type';
 import { Task } from './task.entity';
 
 export interface ITaskRepository {
   create(task: Task): Promise<void>;
+  findAllByUserId(
+    userId: string,
+    filters?: TaskFilters,
+    orderBy?: TaskOrderBy,
+  ): Promise<Task[]>;
 }
