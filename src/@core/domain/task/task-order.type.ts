b4fddf3cdfa5f7adb8fd1b1a73fcf6e52fdf240f
Daniel-Brd <dev.danielbrandao@gmail.com>
2025-07-20 19:36:38 -0300

    [API] feat: domain - add TaskFilters and TaskOrderBy types for task filtering and sorting

---
+++ src/@core/domain/task/task-order.type.ts
@@ -0,0 +1,23 @@
+export enum SortOrder {
+  ASC = 'asc',
+  DESC = 'desc',
+}
+
+export enum NullsOrder {
+  FIRST = 'first',
+  LAST = 'last',
+}
+
+export enum SortBy {
+  CREATED_AT = 'createdAt',
+  DUE_DATE = 'dueDate',
+  STATUS = 'status',
+  TITLE = 'title',
+  DESCRIPTION = 'description',
+}
+
+export type TaskOrderBy = {
+  sortBy: SortBy;
+  sortOrder: SortOrder;
+  nulls?: NullsOrder;
+};
