9535a60128277086d4be175a36872b0bcf92d9e4
Daniel-Brd <dev.danielbrandao@gmail.com>
2025-07-20 14:57:42 -0300

    [API] feat: infra - add jwt token service and strategy

---
+++ src/infra/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,32 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { IUserRepository } from 'src/@core/domain/user';
+
+type JwtPayload = {
+  sub: string;
+  email: string;
+  iat: number;
+  exp: number;
+};
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    configService: ConfigService,
+    @Inject('IUserRepository')
+    private readonly usersRepository: IUserRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET', 'default_secret'),
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.usersRepository.findById(payload.sub);
+    return user;
+  }
+}
