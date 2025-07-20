fa0d030d7213225c827d76875cdf847f41345e13
Daniel-Brd <dev.danielbrandao@gmail.com>
2025-07-20 10:51:56 -0300

    [API] feat: infra - add create user controller

---
+++ src/infra/controllers/users.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUserCommand } from 'src/@core/application/user/commands/create-user.command';
+import { CreateUserUseCase } from 'src/@core/application/user/use-cases/create-user.use-case';
+import {
+  CreateUserInputDto,
+  CreateUserOutputDto,
+} from 'src/@core/contracts/user/create-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly createUserUseCase: CreateUserUseCase) {}
+
+  @Post()
+  async create(@Body() dto: CreateUserInputDto): Promise<CreateUserOutputDto> {
+    const command: CreateUserCommand = {
+      name: dto.name,
+      email: dto.email,
+      password: dto.password,
+    };
+
+    return this.createUserUseCase.execute(command);
+  }
+}
