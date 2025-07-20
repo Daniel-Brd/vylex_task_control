6ea71b7361602434a195c06ae9492a84a4b6cad0
Daniel-Brd <dev.danielbrandao@gmail.com>
2025-07-20 16:04:49 -0300

    [API] feat: add JwtAuthGuard

---
+++ src/guards/jwt-auth.guard.ts
@@ -0,0 +1,18 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor() {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context);
+  }
+
+  getRequest(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest<Request>();
+    return request;
+  }
+}
