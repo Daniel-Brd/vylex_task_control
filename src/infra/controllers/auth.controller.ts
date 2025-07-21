1e1a5f312d65b8a234f977944287d425dfb840f3
Daniel-Brd <dev.danielbrandao@gmail.com>
2025-07-21 20:18:18 -0300

    [API] fix: infra - add Public decorator to login and create user entrypoints

--- src/infra/controllers/auth.controller.ts
+++ src/infra/controllers/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Post, Body, HttpCode } from '@nestjs/common';
 import { AuthenticateUserUseCase } from 'src/@core/application/auth/usecases/auth-user.use-case';
 import { AuthCredentialsInputDto } from 'src/@core/contracts/auth/auth-credentials.dto';
+import { Public } from 'src/decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authUserUseCase: AuthenticateUserUseCase) {}
 
+  @Public()
   @Post('login')
   @HttpCode(200)
   async login(@Body() authCredentialsDto: AuthCredentialsInputDto) {
