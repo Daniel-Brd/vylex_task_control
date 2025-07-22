cd7b61e0e311b8c76598a88424c9a2f62d746a26
Daniel-Brd <dev.danielbrandao@gmail.com>
2025-07-22 20:19:54 -0300

    [WEB] feat: add shared basic form components

---
+++ src/shared/ui/button.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { buttonVariants, type ButtonVariantProps } from './button.variants';
+
+import { cn } from '@/shared/lib/utils';
+
+export function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<'button'> &
+  ButtonVariantProps & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot : 'button';
+
+  return <Comp data-slot="button" className={cn(buttonVariants({ variant, size, className }))} {...props} />;
+}
