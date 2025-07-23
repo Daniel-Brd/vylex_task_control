e3e413d4bd2a1c60c28d1bc5aefcdfd1783fa57c
Daniel-Brd <dev.danielbrandao@gmail.com>
2025-07-23 14:35:59 -0300

    [WEB] feat: add an barell file to task api segment

---
+++ src/entities/task/api/index.ts
@@ -0,0 +1,7 @@
+export { type CreateTaskInputDto } from './dtos/create-task-input.dto';
+export { type CreateTaskOutputDto } from './dtos/create-task-output.dto';
+export { type GetTaskOutputDto } from './dtos/get-task-output.dto';
+export { type GetTaskInputDto } from './dtos/get-tasks-input.dto';
+export { type UpdateTaskInputDto } from './dtos/update-task-input.dto';
+export { type UpdateTaskOutputDto } from './dtos/update-task-output.dto';
+export { mapTaskOutputDtoToTask } from './mappers';
