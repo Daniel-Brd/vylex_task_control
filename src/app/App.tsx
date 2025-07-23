74c25f8dd29696d3f7e29a5c89277d8c0f497089
Daniel-Brd <dev.danielbrandao@gmail.com>
2025-07-23 14:31:05 -0300

    [WEB] feat: provide tanstack query client

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter } from 'react-router';
 import { AppRouter } from './routing/app-router';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="app-container">
-        <main className="content">
-          <AppRouter />
-        </main>
-      </div>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <div className="app-container">
+          <main className="content">
+            <AppRouter />
+          </main>
+        </div>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
