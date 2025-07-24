12ed573baae68bb80587ce6ab7d5ee183bcc6766
Daniel-Brd <dev.danielbrandao@gmail.com>
2025-07-24 03:36:35 -0300

    [WEB] feat: add register user page

--- src/app/routing/app-router.tsx
+++ src/app/routing/app-router.tsx
@@ -4,14 +4,16 @@ import { ProtectedRoute } from './protected-route';
 
 const Login = lazy(() => import('@/pages/login'));
 const TaskBoard = lazy(() => import('@/pages/task-board'));
+const Register = lazy(() => import('@/pages/register'));
 
 export const AppRouter = () => {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route element={<ProtectedRoute />}>
         <Route path="/task-board" element={<TaskBoard />} />
       </Route>
-      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
