149de9f954513e59a1a5316922c06ca5e48fc025
Daniel-Brd <dev.danielbrandao@gmail.com>
2025-07-24 14:21:26 -0300

    [WEB] feat: add get userId from token

--- src/entities/session/ui/auth-provider.tsx
+++ src/entities/session/ui/auth-provider.tsx
@@ -1,16 +1,21 @@
 import { ACCESS_TOKEN_KEY } from '@/shared/api';
 import { useCallback, useEffect, useMemo, useState, type ReactNode } from 'react';
 import { AuthContext } from '../model/context';
+import { jwtDecode } from 'jwt-decode';
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [userId, setUserId] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     try {
       const storedToken = localStorage.getItem(ACCESS_TOKEN_KEY);
+
       if (storedToken) {
         setAccessToken(storedToken);
+        const tokenPayload = jwtDecode(storedToken);
+        setUserId(tokenPayload.sub ?? null);
       }
     } catch (error) {
       console.error('Falha ao acessar o localStorage', error);
@@ -26,18 +31,20 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
   const logout = useCallback(() => {
     localStorage.removeItem(ACCESS_TOKEN_KEY);
+    localStorage.removeItem('user_id');
     setAccessToken(null);
   }, []);
 
   const value = useMemo(
     () => ({
+      userId,
       isAuthenticated: !!accessToken,
       accessToken,
       isLoading,
       login,
       logout,
     }),
-    [accessToken, isLoading, login, logout],
+    [accessToken, isLoading, login, logout, userId],
   );
 
   return <AuthContext value={value}>{children}</AuthContext>;
