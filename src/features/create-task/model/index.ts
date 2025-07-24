1f87c138100a9ba23e968104c50480c44565ac46
Daniel-Brd <dev.danielbrandao@gmail.com>
2025-07-24 15:21:33 -0300

    [WEB] feat: add create task form

---
+++ src/features/create-task/model/index.ts
@@ -0,0 +1,1 @@
+export { type CreateTaskFormData, createTaskSchema } from './schema';
---
+++ src/features/create-task/model/schema.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const createTaskSchema = z.object({
+  title: z.string().min(1, 'Título é obrigatório').max(100, 'Título deve ter no máximo 100 caracteres').trim(),
+  description: z
+    .string()
+    .min(1, 'Descrição é obrigatória')
+    .max(255, 'Descrição deve ter no máximo 255 caracteres')
+    .transform((val) => val?.trim() || ''),
+  dueDate: z.date('Data de prazo é obrigatória').refine((date) => date >= new Date(new Date().setHours(0, 0, 0, 0)), {
+    message: 'Data de prazo não pode ser no passado',
+  }),
+});
+
+export type CreateTaskFormData = z.infer<typeof createTaskSchema>;
