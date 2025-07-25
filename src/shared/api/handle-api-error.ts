5c292dfed0f4032c7d7b47cb5064e053fa873831
Daniel-Brd <dev.danielbrandao@gmail.com>
2025-07-24 22:33:14 -0300

    [WEB] feat: implement error handling for API responses

---
+++ src/shared/api/handle-api-error.ts
@@ -0,0 +1,36 @@
+import { isAxiosError } from 'axios';
+import { toast } from 'sonner';
+
+export const CLIENT_ERROR_CODE = {
+  INTERNAL_ERROR: 'INTERNAL_ERROR',
+  INVALID_CREDENTIALS: 'INVALID_CREDENTIALS',
+  USER_NOT_FOUND: 'USER_NOT_FOUND',
+  TASK_NOT_FOUND: 'TASK_NOT_FOUND',
+  IN_PROGRESS_OR_COMPLETED: 'IN_PROGRESS_OR_COMPLETED',
+  NOT_COMPLETED: 'NOT_COMPLETED',
+  ALREADY_COMPLETED: 'ALREADY_COMPLETED',
+  IS_NOT_OWNER: 'IS_NOT_OWNER',
+} as const;
+
+const COMMOM_MESSAGES: Record<string, string> = {
+  [CLIENT_ERROR_CODE.INTERNAL_ERROR]: 'Ocorreu um erro inesperado.',
+  UNEXPECTED: 'Ocorreu um erro inesperado.',
+};
+
+export type CLIENT_ERROR_CODE = (typeof CLIENT_ERROR_CODE)[keyof typeof CLIENT_ERROR_CODE];
+
+export interface ErrorResponse {
+  statusCode: number;
+  message: string;
+  errorCode: CLIENT_ERROR_CODE;
+}
+
+export const handleApiError = (error: unknown, errorMessages: Record<CLIENT_ERROR_CODE, string>) => {
+  if (!isAxiosError<ErrorResponse>(error) || !error.response?.data?.errorCode) {
+    toast.error(COMMOM_MESSAGES.UNEXPECTED);
+    return;
+  }
+  const clientErrorCode = error.response.data.errorCode;
+  const errorMessage = errorMessages[clientErrorCode] || COMMOM_MESSAGES.UNEXPECTED;
+  toast.error(errorMessage);
+};
